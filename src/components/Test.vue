527e0c7c3147f301268970ec1b1d52f6488a730e
TomasN <gnr202020@gmail.com>
2019-11-14 18:30:53 -0300

    Aprendiendo Vue,js, videos vistos: 4

---
+++ src/components/Test.vue
@@ -0,0 +1,47 @@
+<template>
+<div>
+	<div v-text="title"></div>
+	<div v-if="showName">
+		<p>{{usuario.firstName}}</p>
+	</div>
+	<div v-else>
+	<p>No puedo mostrar el nombre, lo siento.</p>
+</div>
+<hr>
+<ul>
+	<li v-for="task in tasks">
+		{{ task.title }}
+	</li>
+</ul>
+</div>
+
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hola VueJs',
+				usuario: {
+					firstName: 'Juan',
+					lastName: 'Fernandez'
+				},
+				myHtml: '<h3>hola vue</h3',
+				showName: false,
+				tasks: [
+					{title: 'eat'},
+					{title: 'dinner'},
+					{title: 'shower'},
+					{title: 'sleep'}
+				]
+				
+			}
+		}
+
+	}
+</script>
+
+<style>
+	
+
+</style>
